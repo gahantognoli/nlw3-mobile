becaff9b48e8cd8d71d2b8914527d1eaad01145b
Gabriel Antognoli <gabriel_antognoli@hotmail.com>
2020-10-16 10:45:19 -0300

    Day 4

---
+++ src/pages/OrphanageDetails.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const OrphanageDetails = () => {
+    return (
+        <View>
+            
+        </View>
+    )
+}
+
+export default OrphanageDetails
